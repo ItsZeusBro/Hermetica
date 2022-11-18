6f86328d91826ff9bdae3188df2c51991122bb8b
zeus <zeusofolympus1000@gmail.com>
2022-11-18 00:11:32 -0500

    a good example

--- Matricies.js
+++ Matricies.js
@@ -1,10 +1,10 @@
 class Dims {
-	constructor(m, n, d){
+	constructor(d, m, n){
 		//a 2x4 is still a 3 dimensional object or a 2 dimensional object, but not a 1 dimensional object!
 		//what m represents is the length of any given object for every dimension its in
 		//what n represents is the width of any given object for every dimension its in
 		this.m=m
-		this.n=n		
+		this.n=n
 		this.d=d 		//this is the number of planes in the matrix for coordinate information
 	}
 }
@@ -17,11 +17,13 @@ class Cell{
 }
 
 class Matrix {
-	constructor(){
+	constructor(d, m, n){
 		this.matrix = []
+		this.dims=new Dims(d, m, n)
+
 	}
 
-	matrix(dims, matrix=this.matrix){
+	matrix(dims=this.dims, matrix=this.matrix){
 		//we carry over dims.m or dims.n d number of times from 1d identity construction (the smaller of the two) for whichever one is larger 
 		//because that is consistently general throughout (there are no special cases when we take the larger of the two)
 		//so if m=4, and n=3, then a 1 d matrix would be a 1x3 automata. a 2d matrix would be a 1x3x4, a 3d would be 1x3x4x4
@@ -35,3 +37,184 @@ class Matrix {
 
 }
 
+//example:
+//if we have 4, 2, 3
+//then we build it like this:
+//[
+//	
+//]
+
+// d=1
+//[
+//	cell(0), cell(1), cell(2)
+//]
+
+// d=2
+//[
+// [
+//	cell(0, 0), cell(1, 0), cell(2, 0)
+// ],
+// [
+//	cell(0, 1), cell(1, 1), cell(2, 1)
+// ],
+// [
+//	cell(0, 2), cell(1, 2), cell(2, 2)
+// ]
+//]
+
+// d=3
+//[
+	//[
+	// [
+	//	cell(0, 0, 0), cell(1, 0, 0), cell(2, 0, 0)
+	// ],
+	// [
+	//	cell(0, 1, 0), cell(1, 1, 0), cell(2, 1, 0)
+	// ],
+	// [
+	//	cell(0, 2, 0), cell(1, 2, 0), cell(2, 2, 0)
+	// ]
+	//],
+
+	//[
+	// [
+	//	cell(0, 0, 1), cell(1, 0, 1), cell(2, 0, 1)
+	// ],
+	// [
+	//	cell(0, 1, 1), cell(1, 1, 1), cell(2, 1, 1)
+	// ],
+	// [
+	//	cell(0, 2, 1), cell(1, 2, 1), cell(2, 2, 1)
+	// ]
+	//],
+
+	//[
+	// [
+	//	cell(0, 0, 2), cell(1, 0, 2), cell(2, 0, 2)
+	// ],
+	// [
+	//	cell(0, 1, 2), cell(1, 1, 2), cell(2, 1, 2)
+	// ],
+	// [
+	//	cell(0, 2, 2), cell(1, 2, 2), cell(2, 2, 2)
+	// ]
+	//]
+//]
+
+
+// d = 4
+//[
+	//[
+		//[
+		// [
+		//	cell(0, 0, 0, 0), cell(1, 0, 0, 0), cell(2, 0, 0, 0)
+		// ],
+		// [
+		//	cell(0, 1, 0, 0), cell(1, 1, 0, 0), cell(2, 1, 0, 0)
+		// ],
+		// [
+		//	cell(0, 2, 0, 0), cell(1, 2, 0, 0), cell(2, 2, 0, 0)
+		// ]
+		//],
+
+		//[
+		// [
+		//	cell(0, 0, 1, 0), cell(1, 0, 1, 0), cell(2, 0, 1, 0)
+		// ],
+		// [
+		//	cell(0, 1, 1, 0), cell(1, 1, 1, 0), cell(2, 1, 1, 0)
+		// ],
+		// [
+		//	cell(0, 2, 1, 0), cell(1, 2, 1, 0), cell(2, 2, 1, 0)
+		// ]
+		//],
+
+		//[
+		// [
+		//	cell(0, 0, 2, 0), cell(1, 0, 2, 0), cell(2, 0, 2, 0)
+		// ],
+		// [
+		//	cell(0, 1, 2, 0), cell(1, 1, 2, 0), cell(2, 1, 2, 0)
+		// ],
+		// [
+		//	cell(0, 2, 2, 0), cell(1, 2, 2, 0), cell(2, 2, 2, 0)
+		// ]
+		//]
+	//],
+
+	//[
+		//[
+		// [
+		//	cell(0, 0, 0, 1), cell(1, 0, 0, 1), cell(2, 0, 0, 1)
+		// ],
+		// [
+		//	cell(0, 1, 0, 1), cell(1, 1, 0, 1), cell(2, 1, 0, 1)
+		// ],
+		// [
+		//	cell(0, 2, 0, 1), cell(1, 2, 0, 1), cell(2, 2, 0, 1)
+		// ]
+		//],
+
+		//[
+		// [
+		//	cell(0, 0, 1, 1), cell(1, 0, 1, 1), cell(2, 0, 1, 1)
+		// ],
+		// [
+		//	cell(0, 1, 1, 1), cell(1, 1, 1, 1), cell(2, 1, 1, 1)
+		// ],
+		// [
+		//	cell(0, 2, 1, 1), cell(1, 2, 1, 1), cell(2, 2, 1, 1)
+		// ]
+		//],
+
+		//[
+		// [
+		//	cell(0, 0, 2, 1), cell(1, 0, 2, 1), cell(2, 0, 2, 1)
+		// ],
+		// [
+		//	cell(0, 1, 2, 1), cell(1, 1, 2, 1), cell(2, 1, 2, 1)
+		// ],
+		// [
+		//	cell(0, 2, 2, 1), cell(1, 2, 2, 1), cell(2, 2, 2, 1)
+		// ]
+		//]
+	//],
+
+	//[
+		//[
+		// [
+		//	cell(0, 0, 0, 2), cell(1, 0, 0, 2), cell(2, 0, 0, 2)
+		// ],
+		// [
+		//	cell(0, 1, 0, 2), cell(1, 1, 0, 2), cell(2, 1, 0, 2)
+		// ],
+		// [
+		//	cell(0, 2, 0, 2), cell(1, 2, 0, 2), cell(2, 2, 0, 2)
+		// ]
+		//],
+
+		//[
+		// [
+		//	cell(0, 0, 1, 2), cell(1, 0, 1, 2), cell(2, 0, 1, 2)
+		// ],
+		// [
+		//	cell(0, 1, 1, 2), cell(1, 1, 1, 2), cell(2, 1, 1, 2)
+		// ],
+		// [
+		//	cell(0, 2, 1, 2), cell(1, 2, 1, 2), cell(2, 2, 1, 2)
+		// ]
+		//],
+
+		//[
+		// [
+		//	cell(0, 0, 2, 2), cell(1, 0, 2, 2), cell(2, 0, 2, 2)
+		// ],
+		// [
+		//	cell(0, 1, 2, 2), cell(1, 1, 2, 2), cell(2, 1, 2, 2)
+		// ],
+		// [
+		//	cell(0, 2, 2, 2), cell(1, 2, 2, 2), cell(2, 2, 2, 2)
+		// ]
+		//]
+	//],
+//]
