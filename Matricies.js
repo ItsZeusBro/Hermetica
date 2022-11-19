713078785e195cd5fe4526c7c80c62aaaf88bacb
zeus <zeusofolympus1000@gmail.com>
2022-11-19 04:22:25 -0500

    shape could be used for a lot of good things

--- Matricies.js
+++ Matricies.js
@@ -19,18 +19,10 @@ class Matrix {
 		this.d=d
 		this.verify()
 		this.matrix=this.matrix_shell()
-		this.log(this.matrix)
-		console.log(this.count())
 		this.validate()
-		// var pos = []
-		// for(var i=0; i<d; i++){
-		// 	pos.push(0)
-		// }
-		// this.sentinel=new Cell(null, ...pos)
-		// //we need to initialize the matrix with the sentinels initial value
-		//this.init()
-		//console.log(this.matrix)
+		this.load()
 	}
+	
 	verify(){
 		if(this.m>this.n && this.n>=1 && this.d>=2){
 			return true
@@ -55,16 +47,28 @@ class Matrix {
 			throw Error("must have valid dimentions and/or shape")
 		}
 	}
+
 	validate(){
 		//make sure the cell_count is equal to n*m^d
-		console.log(this.n* Math.pow(this.m, this.d-1))
+		//console.log(this.n* Math.pow(this.m, this.d-1))
 		if(this.count()!=(this.n* Math.pow(this.m, this.d-1))){
 			throw Error("Error in validation; there should be ", this.n* Math.pow(this.m, this.d-1), "cells; where m:", this.m, "n:", this.n, "d:", this.d)
 		}
 	}
+
 	count(){
 		return this.flatten(this.matrix).length
 	}
+	shape(){
+		var shape=[]
+		for(var i=0; i<this.d-1; i++){
+			shape.push(this.m)
+		}
+		shape.push(this.n)
+
+		return shape
+	}
+
 	matrix_shell(){
 		var matrix=[]
 		for(var m=0; m<this.m; m++){
@@ -87,9 +91,18 @@ class Matrix {
 			}
 		}
 		//this.matrix=matrix[matrix.length-1]
-	
 		return matrix
 	}
+
+	load(){
+		var shape = this.shape()
+		shape.forEach(element=>{
+			for(var i=0; i<element; i++){
+				
+			}
+		})
+	}
+
 	flatten(matrix, flat=[]){
 		for(var i = 0; i<matrix.length; i++){
 			if(Array.isArray(matrix[i])){
@@ -97,31 +110,21 @@ class Matrix {
 			}else{
 				flat.push(matrix[i])
 			}
-			// if(matrix[i].constructor.name == Cell.constructor.name){
-			// 	flat.push(matrix[i])
-			// 	return flat
-			// }else{
-			// 	flat = this.flatten(matrix[i][matrix[i].length-1], flat)
-			// }
 		}
 		return	flat
 	}
-	_next_cell(){
-		//the next cell takes the sentinel, matrix dimension and shape information,
-		//and updates the sentinel and returns the _next_cell for initialization
-		//this is different than the accessor which iterates over existing cells
 
-		console.log(this.sentinel)
-	}
 
 	log(){
 		console.log(util.inspect(this.matrix, {showHidden: false, depth: null, colors: true}))
-
 	}
-
 }
 
-const matrix = new Matrix(4, 5, 3)
+const matrix = new Matrix(4, 100, 3)
+console.log(matrix.count())
+console.log(matrix.shape())
+//matrix.log()
+
 //matrix.log()
 //matrix._next_cell()
 //example:
