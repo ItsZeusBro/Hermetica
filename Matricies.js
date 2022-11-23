1416652876a219c26af1e8821ae1582dd0f6ae76
zeus <zeusofolympus1000@gmail.com>
2022-11-23 13:11:19 -0500

    we need to make these test cases

--- Matricies.js
+++ Matricies.js
@@ -185,7 +185,15 @@ class Matrix {
 }
 
 const matrix = new Matrix(4, 4, 4)
+matrix.at([0, 0, 0, 0], '0, 0, 0, 0', 'somekey')
+matrix.at([1, 0, 0, 0], '1, 0, 0, 0', 'somekey')
+matrix.at([2, 0, 0, 0], '2, 0, 0, 0', 'somekey')
+matrix.at([3, 0, 0, 0], '3, 0, 0, 0', 'somekey')
+
 matrix.at([0, 0, 0, 1], '0, 0, 0, 1', 'somekey')
+
+matrix.at([3, 2, 1, 0], '3, 2, 1, 0', 'somekey')
+matrix.at([2, 1, 3, 1], '2, 1, 3, 1', 'somekey')
 matrix.at([3, 0, 2, 1], '3, 0, 2, 1', 'somekey')
 
 matrix.log()
