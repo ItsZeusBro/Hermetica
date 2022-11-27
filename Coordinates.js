ade03d5382a046c12902b67642b34abb232c3cb7
zeus <zeusofolympus1000@gmail.com>
2022-11-27 13:14:03 -0500

    min max functionality works for Clock

--- Coordinates.js
+++ Coordinates.js
@@ -1,27 +1,41 @@
 export class Clock{
-	constructor(coordinate1, coordinate2){
+	constructor(coordinate1, coordinate2, min, max){
 		//when it comes to neighbors, we create a +1 and -1 across all dimensions
 		this.coordinate1=coordinate1
 		this.coordinate2=coordinate2
+		this.min=min
+		this.max=max
 		this.previous=null
 	}
 	origin(){
 		return this.coordinate1
 	}
-	max(){
+	_max(){
 		return this.coordinate2
 	}
 	ticks(){
 		var ticks=[]
 		while(true){
 			var next = this.next()
-			if(new Comparator(this.coordinate1.length).isEqual(this.coordinate2, next)){
-				ticks.push(next)
-				return ticks
-			}else{
-				ticks.push(next)
+			if(this.valid(next)){
+				if(new Comparator(this.coordinate1.length).isEqual(this.coordinate2, next)){
+					ticks.push(next)
+					return ticks
+				}else{
+					ticks.push(next)
+				}
 			}
 		}
+	}
+	valid(coordinate){
+		//check to see if any of the coordinates dimensions exceed the limits set by min and max
+		for(var i = 0; i<coordinate.length-1; i++){
+			//console.log(coordinate[i], this.min, this.max)
+			if(coordinate[i]<this.min||coordinate[i]>this.max){
+				return false
+			}
+		}
+		return true
 	}
 	next(){
 		//we wish to increment the coordinate by one step, sometimes that requires incrementing different dimensions
@@ -48,7 +62,7 @@ export class Clock{
 	inc_val(coordinate, i){
 		//console.log(coordinate[i], this.max()[i])
 		//this should return the incremented value of the ith point on the coordinate
-		if(coordinate[i]>=this.max()[i]){
+		if(coordinate[i]>=this._max()[i]){
 			return 0
 		}else{
 			return coordinate[i]+1
@@ -61,7 +75,7 @@ export class Comparator{
 		this.d=d
 	}
 	isEqual(coordinate1, coordinate2){
-		for(var i = 0; i<this.d; i++){
+		for(var i = 0; i<this.d-1; i++){
 			if(coordinate1[i] != coordinate2[i]){
 				return false
 			}
@@ -70,7 +84,7 @@ export class Comparator{
 	}
 
 	isGreater(coordinate1, coordinate2){
-		for(var i = this.d; i>=0; i--){
+		for(var i = this.d-1; i>=0; i--){
 			if(coordinate1[i]>coordinate2[i]){
 				return true
 			}
@@ -88,7 +102,7 @@ export class Comparator{
 	}
 
 	isLess(coordinate1, coordinate2){
-		for(var i = this.d; i>0; i--){
+		for(var i = this.d-1; i>0; i--){
 			if(coordinate1[i]<coordinate2[i]){
 				return true
 			}
@@ -108,4 +122,4 @@ export class Comparator{
 
 }
 
-console.log(new Clock([0,0,0], [2, 2, 2]).ticks())
+console.log(new Clock([-1,-1,-1], [2, 2, 2], 0, 2).ticks())
