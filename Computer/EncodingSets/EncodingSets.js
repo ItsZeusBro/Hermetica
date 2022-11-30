24029704d6d298dec98f2e6dea574ea5dc1873cd
zeus <zeusofolympus1000@gmail.com>
2022-11-30 15:45:11 -0500

    we can compute regex strings from arbitrary input and output strings

--- Computer/EncodingSets/EncodingSets.js
+++ Computer/EncodingSets/EncodingSets.js
@@ -4,7 +4,30 @@
 //to fully encode the input and output for optimal efficiency. In otherwords, we want to strip away
 //extraneous symbols that encode nothing of the underlying string and its expected output.
 class EncodingSets{
-	constructor(){
+	constructor(input, output){
 
 	}
+	arithmetic(input, output){
+		//reduce the string to a minimal encoding map that is a subset of arithmetic symbols that embrace both input and output symbols
+
+
+	}
+	
+	calculus(input, output){
+		//reduce the string to a minimal encoding map that is a subset of calculus symbols that embrace both input and output symbols
+
+	}
+	physics(input, output){
+		//reduce the string to a minimal encoding map that is a subset of physics symbols that embrace both input and output symbols
+
+	}
+	regex(input, output){
+		//reduce the string to a minimal encoding map that is a subset of regex symbols that embrace both input and output symbols
+
+	}
+	english(input, output){
+		//reduce the string to a minimal encoding map that is a subset of english symbols that embrace both input and output symbols
+
+	}
+
 }
