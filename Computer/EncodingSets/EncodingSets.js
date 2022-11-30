88625014d1aecb9978bac69b4b886412e111dd65
zeus <zeusofolympus1000@gmail.com>
2022-11-30 15:28:09 -0500

    optimally efficient encoding definition

--- Computer/EncodingSets/EncodingSets.js
+++ Computer/EncodingSets/EncodingSets.js
@@ -1,5 +1,8 @@
 //This is responsible for encoding strings to symbols accepted by automata, computer, and rules
 //these strings are categorized by a minimal superset of codes that fully encode the string
+//It doesnt even have to be the entire encoding superset, it just has to embrace enough of the superset
+//to fully encode the input and output for optimal efficiency. In otherwords, we want to strip away
+//extraneous symbols that encode nothing of the underlying string and its expected output.
 class EncodingSets{
 	constructor(){
 
