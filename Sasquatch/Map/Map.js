5c0351afa97043fd1bed9b64c386fbe19e51e8d4
Zeus <zeusofolympus1000@gmail.com>
2022-12-04 19:21:40 -0500

    more refactoring

---
+++ Sasquatch/Map/Map.js
@@ -0,0 +1,96 @@
+// import util from 'node:util'
+import {Utils} from "../../Utils.js"
+import { CodeMap } from "./CodeMap/CodeMap.js"
+import {CoordinateClock} from "../Matrix/Coordinates.js"
+import {Neighborhood} from "./Neighborhood/Neighborhood.js"
+import {createHash} from 'node:crypto'
+import {RuleTree} from "./RuleTree/RuleTree.js"
+import fs from "node:fs"
+
+//REMEMBER when REFRESHING a RuleSystem, we dont have to recreate all of the components
+//This optimizes load times between simulations which can be significant
+export class Map{
+	constructor(input, output, context, dimension){
+		this.map = new CodeMap(input, output, context).map
+		this.map['dimension']=dimension
+		this.map['omega']=map['codes'].length
+		this._coordinates(this.map)
+		//1-2 neighbors for 1 dimension; 2-4 for 2 dimensions; 3-6 for 3 dimensions;  4-8 for 4 dimensions 
+		//new RuleTree(this.map)
+	}
+
+
+
+	//public api
+
+	shape(){
+		
+	}
+
+	neighborhood(dimension){
+		
+	}
+
+	neighbors(dimension, coordinate){
+
+	}
+
+	coordinates(dimension){
+
+	}
+
+
+
+	input(){
+		return this.map['inputVector']
+	}
+
+	output(){
+		return this.map['outputVector']
+	}
+	log(element){
+		//console.log(this.map)
+		if(element){
+			console.log(util.inspect(element, {showHidden: false, depth: 3, colors: true}))
+
+		}else{
+			console.log(util.inspect(this.map, {showHidden: false, depth: 3, colors: true}))
+		}
+	}
+	//setup functions
+	_coordinates(map){
+		var ruleKeys = Object.keys(map['rules'])
+		for(var j = 0; j<ruleKeys.length; j++){
+			var d = parseInt([ruleKeys[j]])
+			var c1=[]
+			var c2=[]
+			for(var i = 0; i<d; i++){
+				c1.push(0)
+				c2.push(parseInt(map['rules'][ruleKeys[j]]['shape'])-1)
+			}
+			var coordinates = new CoordinateClock(c1, c2).coordinates()
+			for(var i = 0; i<coordinates.length; i++){
+				coordinates[i]=coordinates[i].join(',')
+			}
+			map['rules'][ruleKeys[j]]['coordinates']=coordinates
+		}
+	}
+
+	hash(map){
+		//if we hash the relevant details of a simulation and store the hash alongside it
+		//we can do a quick lookup to see if the problem was already solved ensuring we dont
+		//waste compute time. A hash is particular to a specific input, output, and context
+		//it is not particular to a group of test cases, just one of them
+		var ruleKeys = Object.keys(map['rules'])
+		for(var i = 0; i<ruleKeys.length; i++){
+			var rule = JSON.stringify(map['rules'][ruleKeys[i]]['neighborhood'])
+			map['rules'][ruleKeys[i]]['nhHash']=createHash('sha256').update(rule).digest('hex'); 
+		}
+		
+	}
+}
+
+var map = new Map('abcdefghijklmn', 'abcdefghijklmn', 'english', [1, 2, 3])
+map.log()
+
+
---
+++ Sasquatch/Map/Matrix/Coordinates.js
@@ -0,0 +1,126 @@
+export class CoordinateClock{
+	constructor(coordinate1, coordinate2, min, max){
+		//when it comes to neighbors, we create a +1 and -1 across all dimensions
+		this.coordinate1=coordinate1
+		this.coordinate2=coordinate2
+		this.min=min
+		this.max=max
+		this.previous=null
+	}
+	_min(){
+		return this.coordinate1
+	}
+	_max(){
+		return this.coordinate2
+	}
+	coordinates(){
+		var coordinates=[]
+		while(true){
+			var next = this.next()
+			if(next&&this.valid(next)){
+				if(new Comparator(this.coordinate1.length).isEqual(this.coordinate2, next)){
+					coordinates.push(next)
+					return coordinates
+				}else{
+					coordinates.push(next)
+				}
+			}else{
+				return coordinates
+			}
+			
+		}
+	}
+	valid(coordinate){
+		//check to see if any of the coordinates dimensions exceed the limits set by min and max
+		for(var i = 0; i<coordinate.length-1; i++){
+			//console.log(coordinate[i], this.min, this.max)
+			if(coordinate[i]<this.min||coordinate[i]>this.max){
+				return false
+			}
+		}
+		return true
+	}
+	next(){
+		//we wish to increment the coordinate by one step, sometimes that requires incrementing different dimensions
+		//if we call increment_val on a particular dimension and it returns 0, we need to increment_val on the next dimension
+		if(this.previous==null){
+			this.previous=this._min()
+			return this.previous
+		}
+		var current=JSON.parse(JSON.stringify(this.previous))
+		for(var i = 0; i<this.previous.length; i++){
+			if(this.inc_val(current, i)){
+				current[i]=this.inc_val(current, i)
+				break
+			}else{
+				//in this case it returns zero, and we need to set the ith index to 0, and increment the next (iterate)	
+				current[i]=0;		
+			}
+		}
+		this.previous=current
+		return current
+	}
+
+
+	inc_val(coordinate, i){
+		//console.log(coordinate[i], this.max()[i])
+		//this should return the incremented value of the ith point on the coordinate
+		if(coordinate[i]>=this._max()[i]){
+			return 0
+		}else{
+			return coordinate[i]+1
+		}
+	}
+}
+
+export class Comparator{
+	constructor(d){
+		this.d=d
+	}
+	isEqual(coordinate1, coordinate2){
+		for(var i = 0; i<coordinate1.length; i++){
+			if(coordinate1[i] != coordinate2[i]){
+				return false
+			}
+		}
+		return true
+	}
+
+	isGreater(coordinate1, coordinate2){
+		for(var i = coordinate1.length-1; i>=0; i--){
+			if(coordinate1[i]>coordinate2[i]){
+				return true
+			}
+		}
+		return false
+	}
+
+	isGreaterEqual(coordinate1, coordinate2){
+		if(this.isEqual(coordinate1, coordinate2)&& this.isGreater(coordinate1, coordinate2)){
+			return true
+		}else{
+			return false
+		}
+
+	}
+
+	isLess(coordinate1, coordinate2){
+		for(var i = coordinate1.length-1; i>=0; i--){
+			if(coordinate1[i]<coordinate2[i]){
+				return true
+			}
+		}
+		return false
+	}
+
+	isLessEqual(coordinate1, coordinate2){
+		if(this.isEqual(coordinate1, coordinate2)&& this.isLess(coordinate1, coordinate2)){
+			return true
+		}else{
+			return false
+		}
+	}
+
+}
+
+// console.log(new Clock([-1,-1,-1], [2, 2, 2], 0, 2).coordinates())
