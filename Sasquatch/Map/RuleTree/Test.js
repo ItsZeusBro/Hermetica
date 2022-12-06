43a64132be288fb07de9038c94eeb5ca6cd3666a
Zeus <zeusofolympus1000@gmail.com>
2022-12-06 01:04:59 -0500

    mathematics check out for Combinations with Repetitions for Neighborhoods in tests

--- Sasquatch/Map/RuleTree/Test.js
+++ Sasquatch/Map/RuleTree/Test.js
@@ -10,15 +10,17 @@ class Test{
     }
 
     tests(){
-        this.ruleTree()
-        this.nextNeighborhood()
-        this.treeInsert()
-        this.refresh()
-        this.randomRule()
-        this.rule()
-        this.exists()
-        this.exprt()
-        this.import()
+        // this.ruleTree()
+        // this.neighborhoods()
+        this.nNeighborhoods()
+        // this.nextNeighborhood()
+        // this.treeInsert()
+        // this.refresh()
+        // this.randomRule()
+        // this.rule()
+        // this.exists()
+        // this.exprt()
+        // this.import()
     }
 
     ruleTree(){
@@ -39,7 +41,6 @@ class Test{
         }
 
         for(var m=0; m<=dims-1; m++){
-            maps[m].log()
             var rules = maps[m].map['rules'].slice()
             for(var i=0; i<maps[m].map['neighborhoods'].length; i++){
                 var message='neighborhoods for both sub-maps should have the same chars'
@@ -63,21 +64,62 @@ class Test{
         for(var i = 0; i<arr1.length; i++){
             assert.equal(arr1[i], arr2[i], message)
         }
-        
     }
     neighborhoods(){
         //we need some math, because the number of neighborhoods is not a function of the number of cells
         //it is a function of the number of unique encodings 
+
         var maps=[]
-        var dims=2
+        var dims=5
         for(var i = 1; i<=dims; i++){
-            var map = new Map('abcdef', 'abcdef', 'english', i)
-            new RuleTree().neighorhoods()
+            var map = new Map('a', 'a', 'english', i)
+            new RuleTree().ruleTree(map.map)
             maps.push(map)
+            maps[i-1].log()
+            console.log(maps[i-1].map['nNeighborhoods'])
         }
 
     }
 
+    nNeighborhoods(){
+        var strings=[]
+        var string=""
+
+        for(var i = 1; i<=5; i++){
+
+            string+=i
+            strings.push(string)
+        }
+
+        for(var d = 1; d<=5; d++){
+            for(var i = 0; i<5; i++){
+                var s = strings[i]
+                var map = new Map(s, s, 'english', d)
+                new RuleTree().ruleTree(map.map)
+                // map.log()
+                console.log(map.map['nNeighborhoods'],this._nNeighborhoods(s.length+1, d))
+                console.log(map.map['nNeighborhoods']==this._nNeighborhoods(s.length+1, d))    
+            }
+            
+        }
+      
+
+    }
+    _nNeighborhoods(uniqueChars, dimension){
+        var r = uniqueChars
+        var nNeighborhoods=0
+        for(var n = dimension; n<=2*dimension; n++){
+            nNeighborhoods+=(this.factorial((n+r-1)))/(this.factorial(n)*this.factorial(r-1))
+        }
+        return nNeighborhoods
+    }
+    factorial (n) {
+        //https://stackoverflow.com/questions/3959211/what-is-the-fastest-factorial-function-in-javascript
+        if (n === 0)
+        { return 1; }
+        else
+        { return n * this.factorial( n - 1 ); }
+    }
     nextNeighborhood(){
 
     }
