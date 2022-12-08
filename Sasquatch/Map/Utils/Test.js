b59d59a71a13daf0eee7be521d090e160b4d007d
Zeus <zeusofolympus1000@gmail.com>
2022-12-08 11:37:31 -0500

    tests complete

--- Sasquatch/Map/Utils/Test.js
+++ Sasquatch/Map/Utils/Test.js
@@ -1,33 +1,42 @@
 import {Combinatorics} from "./Combinatorics.js"
 import assert from "node:assert"
 class Test{
-    constructor(){
-        this.tests()
+    constructor(n){
+        this.tests(n)
     }
-    tests(){
-        this.PwithR()
-        this.PwithoutR()
-        this.CwithR()
+    tests(n){
+        var symbols=[]
+        var n_s=[]
+        for(var i = 1; i<=n; i++){
+            symbols.push(i)
+            n_s.push(i)
+        }
+        for(var i = 0; i<n_s.length; i++){
+            this.PwithR(symbols, n_s[i])
+            this.PwithoutR(symbols, n_s[i])
+            this.CwithR(symbols, n_s[i])
+        }
+
 
     }
-    PwithR(){
-        var result = new Combinatorics().PwithR([1, 2, 3, 4, 5], 4)
+    PwithR(symbols, n){
+        var result = new Combinatorics().PwithR(symbols, n)
         console.log(result)
-        this.uniquePwithR([1, 2, 3, 4, 5].length, 4, result)
+        this.uniquePwithR(symbols.length, n, result)
 
     }
-    PwithoutR(){
-        var result = new Combinatorics().PwithoutR([1, 2, 3, 4, 5], 4)
-        this.uniquePwithoutR([1, 2, 3, 4, 5].length, 4, result)
-
+    PwithoutR(symbols, n){
+        var result = new Combinatorics().PwithoutR(symbols, n)
         console.log(result)
-    }
-    CwithR(){
-        var result = new Combinatorics().CwithR([1, 2, 3, 4, 5], 4)
-        console.log(result)
-        this.uniqueCwithR([1, 2, 3, 4, 5].length, 4, result)
+        this.uniquePwithoutR(symbols.length, n, result)
 
     }
+    CwithR(symbols, n){
+        var result = new Combinatorics().CwithR(symbols, n)
+        console.log(result)
+        this.uniqueCwithR(symbols.length, n, result)
+    }
+
     CwithoutR(){
         //still need to write function
     }
@@ -37,19 +46,16 @@ class Test{
             var str = result[i].join("")
             set.add(str)
         }
-    
         assert.equal(set.size==new Combinatorics()._PwithR(n, r), true)
-
     }
 
     uniquePwithoutR(n, r, result){
         var set = new Set()
-        for(var i =0; i<result.length; i++){
+        for(var i=0; i<result.length; i++){
             var str = result[i].join("")
             set.add(str)
         }
         assert.equal(set.size==new Combinatorics()._PwithoutR(n, r), true)
-
     }
 
     uniqueCwithR(n, r, result){
@@ -62,4 +68,4 @@ class Test{
     }
 }
 
-new Test()
+new Test(7)
