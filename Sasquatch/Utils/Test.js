638123867581f2c9ec598a73aea008f9ca69fb88
Zeus <zeusofolympus1000@gmail.com>
2022-12-13 19:34:19 -0500

    we are using a different approach

--- Sasquatch/Utils/Test.js
+++ Sasquatch/Utils/Test.js
@@ -7,8 +7,8 @@ export class UtilsTest{
         // this.bin2hex()
 
         // this.string2Hex()
-        this.hex2String()
-        this._buffer2String()
+        // this.hex2String()
+        // this._buffer2String()
         // this.string2Buffer()
         // this.buffer2String()
         
@@ -20,191 +20,231 @@ export class UtilsTest{
 
         // this.hex2Decimal()
         // this.decimal2Hex()
+        this.range()
+        this.str()
 
     }
-    _buffer2String(){
-        var buff = new Encoding().string2Buffer('hello world!')
-        var string = new Encoding()._buffer2String(buff)
-        console.log(string)
-    }
-    hex2bin(){
-        console.log('hex2bin()')
-        for(var i = 1; i<200; i++){
-            var hex = new Encoding().hex(i)
-            var bin = new Encoding().hex2bin(hex)
-            assert.equal(hex, new Encoding().bin2hex(bin))
-        }
-    }
-    bin2hex(){
-        console.log('bin2hex()')
-        for(var i = 0; i<200; i++){
-            var bytes = new Encoding().bytes(i)
-            var hex = new Encoding().bin2hex(bytes)
-            assert.equal(bytes, new Encoding().hex2bin(hex))
-        }
-    }
-
-    string2Hex(){
-        console.log('string2hex()')
-        for(var i = 10; i<200; i++){
-            var str = new Rand().str(i)
-            var hex = new Encoding().string2Hex(str)
-            console.log('a;lgkas;dglk', str, hex)
-            assert.equal(str, new Encoding().hex2String(new Encoding().strip(hex)))
-        }
-    }
-
-    hex2String(){
-        console.log('hex2String()')
-        for(var i = 1; i<100; i++){
-            if(i%2==0){
-                var hex = new Encoding().hex(i)
-                var str = new Encoding().hex2String(hex)  
-                console.log(hex, str)  
-                assert.equal(hex, new Encoding().string2Hex(str))
-            }
-            
-        }
-    }
-
-    string2Buffer(){
-        console.log('string2Buffer()')
-        for(var i = 0; i<200; i++){
-            var str = new Rand().str(i)
-            var buffer = new Encoding().string2Buffer(str)
-            assert.equal(str, new Encoding().buffer2String(buffer))
-        }
-    }
-    buffer2String(){
-        console.log('buffer2String()')
-        for(var i = 1; i<200; i++){
-            if(i%2==0){
-                var buffer = new Rand().buffer(i)
-                var str = new Utils().buffer2String(buffer)
-                for(var j = 0; j<buffer.length; j++){
-                    assert.equal(buffer[j], new Utils().string2Buffer(str)[j])
+    str(){
+        for(var i = 0; i<10; i++){
+            var inclusive=false
+            var j = 100;
+            while(inclusive==false){
+                j++
+                var str = new Rand().str(i, j)
+                console.log(str)
+                assert.equal(str.length<=j, true)
+                assert.equal(str.length>=i, true)
+                if(str.length==j||str.length==i){
+                    inclusive=true
+                    break
                 }
             }
+            assert.equal(inclusive, true)
         }
     }
-
-    buffer2Hex(){
-        console.log('buffer2Hex()')
-        for(var i = 0; i<200; i++){
-            var hex = new Rand().hex(i)
-            var buffer = new Utils().hex2Buffer(hex)
-            assert.equal(hex, new Utils().buffer2Hex(buffer))
-        }
-    }
-
-    hex2Buffer(){
-        console.log('hex2Buffer()')
-        for(var i = 0; i<200; i++){
-            var buffer = new Rand().buffer(i)
-            var hex = new Utils().buffer2Hex(buffer)
-            for(var j=0; j<buffer.length; j++){
-                assert.equal(buffer[j], new Utils().hex2Buffer(hex)[j])
-            }
-        }
-    }
-
-    bin2Decimal(){
-        console.log('bin2Decimal()')
-        for(var i = 1; i<200; i++){
-            var bytes = new Rand().bytes(5)
-            assert.equal(parseInt(bytes, 2), new Utils().bin2Decimal(bytes))
-        }
-    }
-
-    decimal2Bin(){
-        console.log('decimal2Bin()')
-        for(var i = 0; i<10000; i++){
-            var decimal = new Rand().int(i)
-            var bin = new Utils().decimal2Bin(decimal)
-            assert.equal(decimal, new Utils().bin2Decimal(bin))
-        }
-    }  
-
-    hex2Decimal(){
-        console.log('hex2Decimal()')
-        for(var i = 5; i<20; i++){
-
-            var hex = new Rand().hex(i)
-            var decimal = new Utils().hex2Decimal(hex)
-            // console.log(hex, decimal)
-            assert.equal(new Utils().hex2bin(hex), new Utils().decimal2Bin(decimal))
-        }
-    }
-
-    decimal2Hex(){
-        console.log('decimal2Hex()')
-        for(var i = 0; i<10000; i++){
-            var hex = new Rand().hex(i)
-            var decimal = new Utils().hex2Decimal(hex)
-            // console.log(hex, decimal)
-            assert.equal(hex, new Utils().decimal2Hex(decimal))
-        }
-    }
-
-    objectComparator(){
-
-    }
-
-    str(){
-
-    }
-
-    int(){
-
-    }
-
-    arr(){
-
-    }
-
-    thing(){
-
-    }
-
-    intArr(){
-
-    }
-
-    strArr(){
-
-    }
-
-    obj(){
-
-    }
-
-    objArr(){
-
-    }
-
-    selection(){
-
-    }
-
     range(){
-
+        for(var i = 0; i<10; i++){
+            var inclusive=false
+            var j = 100;
+            while(inclusive==false){
+                j++
+                var number = new Rand().range(i, j)
+                console.log(number)
+                assert.equal(number<=j, true)
+                assert.equal(number>=i, true)
+                if(number==j||number==i){
+                    inclusive=true
+                    break
+                }
+            }
+            assert.equal(inclusive, true)
+        }
     }
 
-    mod10(){
-
+    decimal2BinBE(){
+        
     }
+    // _buffer2String(){
+    //     var buff = new Encoding().string2Buffer('hello world!')
+    //     var string = new Encoding()._buffer2String(buff)
+    //     console.log(string)
+    // }
+    // hex2bin(){
+    //     console.log('hex2bin()')
+    //     for(var i = 1; i<200; i++){
+    //         var hex = new Encoding().hex(i)
+    //         var bin = new Encoding().hex2bin(hex)
+    //         assert.equal(hex, new Encoding().bin2hex(bin))
+    //     }
+    // }
+    // bin2hex(){
+    //     console.log('bin2hex()')
+    //     for(var i = 0; i<200; i++){
+    //         var bytes = new Encoding().bytes(i)
+    //         var hex = new Encoding().bin2hex(bytes)
+    //         assert.equal(bytes, new Encoding().hex2bin(hex))
+    //     }
+    // }
 
-    latin(){
+    // string2Hex(){
+    //     console.log('string2hex()')
+    //     for(var i = 10; i<200; i++){
+    //         var str = new Rand().str(i)
+    //         var hex = new Encoding().string2Hex(str)
+    //         console.log('a;lgkas;dglk', str, hex)
+    //         assert.equal(str, new Encoding().hex2String(new Encoding().strip(hex)))
+    //     }
+    // }
 
-    }
+    // hex2String(){
+    //     console.log('hex2String()')
+    //     for(var i = 1; i<100; i++){
+    //         if(i%2==0){
+    //             var hex = new Encoding().hex(i)
+    //             var str = new Encoding().hex2String(hex)  
+    //             console.log(hex, str)  
+    //             assert.equal(hex, new Encoding().string2Hex(str))
+    //         }
+            
+    //     }
+    // }
 
-    arabic(){
+    // string2Buffer(){
+    //     console.log('string2Buffer()')
+    //     for(var i = 0; i<200; i++){
+    //         var str = new Rand().str(i)
+    //         var buffer = new Encoding().string2Buffer(str)
+    //         assert.equal(str, new Encoding().buffer2String(buffer))
+    //     }
+    // }
+    // buffer2String(){
+    //     console.log('buffer2String()')
+    //     for(var i = 1; i<200; i++){
+    //         if(i%2==0){
+    //             var buffer = new Rand().buffer(i)
+    //             var str = new Utils().buffer2String(buffer)
+    //             for(var j = 0; j<buffer.length; j++){
+    //                 assert.equal(buffer[j], new Utils().string2Buffer(str)[j])
+    //             }
+    //         }
+    //     }
+    // }
 
-    }
+    // buffer2Hex(){
+    //     console.log('buffer2Hex()')
+    //     for(var i = 0; i<200; i++){
+    //         var hex = new Rand().hex(i)
+    //         var buffer = new Utils().hex2Buffer(hex)
+    //         assert.equal(hex, new Utils().buffer2Hex(buffer))
+    //     }
+    // }
 
-    cjk(){
+    // hex2Buffer(){
+    //     console.log('hex2Buffer()')
+    //     for(var i = 0; i<200; i++){
+    //         var buffer = new Rand().buffer(i)
+    //         var hex = new Utils().buffer2Hex(buffer)
+    //         for(var j=0; j<buffer.length; j++){
+    //             assert.equal(buffer[j], new Utils().hex2Buffer(hex)[j])
+    //         }
+    //     }
+    // }
 
-    }
+    // bin2Decimal(){
+    //     console.log('bin2Decimal()')
+    //     for(var i = 1; i<200; i++){
+    //         var bytes = new Rand().bytes(5)
+    //         assert.equal(parseInt(bytes, 2), new Utils().bin2Decimal(bytes))
+    //     }
+    // }
+
+    // decimal2Bin(){
+    //     console.log('decimal2Bin()')
+    //     for(var i = 0; i<10000; i++){
+    //         var decimal = new Rand().int(i)
+    //         var bin = new Utils().decimal2Bin(decimal)
+    //         assert.equal(decimal, new Utils().bin2Decimal(bin))
+    //     }
+    // }  
+
+    // hex2Decimal(){
+    //     console.log('hex2Decimal()')
+    //     for(var i = 5; i<20; i++){
+
+    //         var hex = new Rand().hex(i)
+    //         var decimal = new Utils().hex2Decimal(hex)
+    //         // console.log(hex, decimal)
+    //         assert.equal(new Utils().hex2bin(hex), new Utils().decimal2Bin(decimal))
+    //     }
+    // }
+
+    // decimal2Hex(){
+    //     console.log('decimal2Hex()')
+    //     for(var i = 0; i<10000; i++){
+    //         var hex = new Rand().hex(i)
+    //         var decimal = new Utils().hex2Decimal(hex)
+    //         // console.log(hex, decimal)
+    //         assert.equal(hex, new Utils().decimal2Hex(decimal))
+    //     }
+    // }
+
+    // objectComparator(){
+
+    // }
+
+
+
+    // int(){
+
+    // }
+
+    // arr(){
+
+    // }
+
+    // thing(){
+
+    // }
+
+    // intArr(){
+
+    // }
+
+    // strArr(){
+
+    // }
+
+    // obj(){
+
+    // }
+
+    // objArr(){
+
+    // }
+
+    // selection(){
+
+    // }
+
+    // range(){
+
+    // }
+
+    // mod10(){
+
+    // }
+
+    // latin(){
+
+    // }
+
+    // arabic(){
+
+    // }
+
+    // cjk(){
+
+    // }
 } 
 
 new UtilsTest()
--- Sasquatch/Utils/Utils.js
+++ Sasquatch/Utils/Utils.js
@@ -45,34 +45,17 @@ export class Rand{
     constructor(){
         this.rand=this
     }
-    str(n){return this.rand._str(this.rand.range(0, n))}
-    int(n){return this.rand.range(0,n)}
-    arr(n){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand.thing())}; return arr}
-    thing(){
-        return[
-            this.rand.intArr, this.rand.str, this.rand.int, this.rand.enc, this.rand.encArr, this.rand.strArr,
-            this.rand.obj, this.rand.objArr
-        ].sample()()
-    }
-    intArr(n=this.rand.int()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand.int())}; return arr}
 
-    strArr(n=this.rand.int()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand.str())}; return arr}
-    obj(n=this.rand.int()){if(n){return {[this.rand.str()]:this.rand.obj(n-1)}}else{return {}}};
-    objArr(n=this.rand.int()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand.obj())}; return arr}
-    selection(bag){
-        return bag[Math.floor(Math.random() * bag.length)];
-    }
-    range(min, max){
-        return Math.floor(Math.random()*(max-min+1)+min)
-    }
+	range(min, max){
+		return Math.floor(Math.random() * (max - min + 1) + min)
+	}
+    str(min, max){return this.rand._str(this.rand.range(min, max))}
+
     _str(len, chars=this.latin().join('')){
         //programiz.com
         var str='';
         for (var i = 0; i<len; i++){str+=chars.charAt(Math.floor(Math.random()*chars.length))}
         return str;
-    }
-    mod10(){
-        return Math.floor(Math.random()*(100-0+1)+0)%2
     }
 	latin(){
 		var latin =[] 
@@ -81,115 +64,109 @@ export class Rand{
 		}
 		return latin
 	}
-	arabic(){
-		var arabic =[] 
-		for(var i = 1536; i<=1791; i++){
-			arabic.push(String.fromCharCode(i+''))
+	string2Hex(string){
+		string=string.slice()
+		for(var i = 0; i<string.length; i++){
+
 		}
-		return arabic
+		return 
+	}
+
+//     int(n){return this.rand.range(0,n)}
+//     arr(n){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand.thing())}; return arr}
+//     thing(){
+//         return[
+//             this.rand.intArr, this.rand.str, this.rand.int, this.rand.enc, this.rand.encArr, this.rand.strArr,
+//             this.rand.obj, this.rand.objArr
+//         ].sample()()
+//     }
+//     intArr(n=this.rand.int()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand.int())}; return arr}
+
+//     strArr(n=this.rand.int()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand.str())}; return arr}
+//     obj(n=this.rand.int()){if(n){return {[this.rand.str()]:this.rand.obj(n-1)}}else{return {}}};
+//     objArr(n=this.rand.int()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand.obj())}; return arr}
+//     selection(bag){
+//         return bag[Math.floor(Math.random() * bag.length)];
+//     }
+
+
+//     mod10(){
+//         return Math.floor(Math.random()*(100-0+1)+0)%2
+//     }
+
+// 	arabic(){
+// 		var arabic =[] 
+// 		for(var i = 1536; i<=1791; i++){
+// 			arabic.push(String.fromCharCode(i+''))
+// 		}
+// 		return arabic
 	
-	}
+// 	}
 
-	cjk(){
-		var cjk =[] 
-		for(var i = 19968; i<=20168; i++){
-			'\u{1F603}'
-			cjk.push(String.fromCharCode(i+''))
-		}
-		return cjk
-	}
-	algebra(){
+// 	cjk(){
+// 		var cjk =[] 
+// 		for(var i = 19968; i<=20168; i++){
+// 			'\u{1F603}'
+// 			cjk.push(String.fromCharCode(i+''))
+// 		}
+// 		return cjk
+// 	}
+// 	algebra(){
 
-	}
-	calculus(){
+// 	}
+// 	calculus(){
 
-	}
-	physics(){
+// 	}
+// 	physics(){
 
-	}
+// 	}
 }
 
 export class Encoding{
-
-	hex2bin(hex){
-		//https://stackoverflow.com/questions/45053624/convert-hex-to-binary-in-javascript
-		var out = "";
-		hex = this.strip(hex.slice())
-		for(var c of hex) {
-			switch(c) {
-				case '0': out += "0000"; break;
-				case '1': out += "0001"; break;
-				case '2': out += "0010"; break;
-				case '3': out += "0011"; break;
-				case '4': out += "0100"; break;
-				case '5': out += "0101"; break;
-				case '6': out += "0110"; break;
-				case '7': out += "0111"; break;
-				case '8': out += "1000"; break;
-				case '9': out += "1001"; break;
-				case 'a': out += "1010"; break;
-				case 'b': out += "1011"; break;
-				case 'c': out += "1100"; break;
-				case 'd': out += "1101"; break;
-				case 'e': out += "1110"; break;
-				case 'f': out += "1111"; break;
-				default: return "";
-			}
-		}
-		return this.binFormat(out);
+	nibbleFormatBE(bin){
+		//this assumes Big Endianes binary number as input, so no information
+		//is lost. It returns a Big Endian nibble
+		return bin.slice(-4)
 	}
-	bin2hex(bin){
-		// console.log('pre-format:',bin)
-		bin = this.binFormat(bin.slice())
-		// console.log('post-format:',bin)
-		var out = "";
-		var accumulator=''
-		for(var c = 1; c<=bin.length; c++) {
-			accumulator+=bin[c-1]
-			if(c%4==0){
-				switch(accumulator) {
-					case "0000": out += '0'; break;
-					case "0001": out += '1'; break;
-					case "0010": out += '2'; break;
-					case "0011": out += '3'; break;
-					case "0100": out += '4'; break;
-					case "0101": out += '5'; break;
-					case "0110": out += '6'; break;
-					case "0111": out += '7'; break;
-					case "1000": out += '8'; break;
-					case "1001": out += '9'; break;
-					case "1010": out += 'a'; break;
-					case "1011": out += 'b'; break;
-					case "1100": out += 'c'; break;
-					case "1101": out += 'd'; break;
-					case "1110": out += 'e'; break;
-					case "1111": out += 'f'; break;
-					default: return "";
-				}
-				// console.log(accumulator)
-				accumulator=""
-			}
-		}
-		// console.log(out)
-		return this.strip(out);
+	nibbleFormatLE(bin){
+		//this assumes Little Endianes binary number as input, so no information
+		//is lost. It returns a Little Endian nibble
+		return bin.slice(0, 4)
 	}
-	bin2Decimal(bin){
-		//start from the right
-		bin=this.binFormat(bin.slice())
-		var i = bin.length-1;
-		var decimal=0
-		var j = 0;
-		while(i>=0){
-			if(bin[i]=='1'){
-				decimal+=Math.pow(2, j)
-			}
-			i--
-			j++
+	
+	_decimal2BinBE(decimal){
+		if(decimal<0||decimal>9){
+			throw Error('_decimal2Bin does not recognize numbers less than 0 or greater than 9')
 		}
-		return decimal
+		switch (decimal) {
+			case 0: return '0';
+			case 1: return '1';
+			case 2: return '10';
+			case 3: return '11';
+			case 4: return '100';
+			case 5: return '101';
+			case 6: return '110';
+			case 7: return '111';
+			case 8: return '1000';
+			case 9: return '1001';
+		  }
 	}
-
-	decimal2Bin(decimal){
+	_decimal2BinLE(decimal){
+		if(decimal<0||decimal>9){
+			throw Error('_decimal2Bin does not recognize numbers less than 0 or greater than 9')
+		}
+		case 0: return '0';
+		case 1: return '1';
+		case 2: return '01';
+		case 3: return '11';
+		case 4: return '001';
+		case 5: return '101';
+		case 6: return '011';
+		case 7: return '111';
+		case 8: return '0001';
+		case 9: return '1001';
+	}
+	decimal2BinBE(decimal){
 		var bin=""
 		var i = 0
 		while(true){
@@ -218,144 +195,223 @@ export class Encoding{
 			}
 			i--
 		}
-		
 		return this.binFormat(bin)
 	}
+// 	hex2bin(hex){
+// 		//https://stackoverflow.com/questions/45053624/convert-hex-to-binary-in-javascript
+// 		var out = "";
+// 		hex = this.strip(hex.slice())
+// 		for(var c of hex) {
+// 			switch(c) {
+// 				case '0': out += "0000"; break;
+// 				case '1': out += "0001"; break;
+// 				case '2': out += "0010"; break;
+// 				case '3': out += "0011"; break;
+// 				case '4': out += "0100"; break;
+// 				case '5': out += "0101"; break;
+// 				case '6': out += "0110"; break;
+// 				case '7': out += "0111"; break;
+// 				case '8': out += "1000"; break;
+// 				case '9': out += "1001"; break;
+// 				case 'a': out += "1010"; break;
+// 				case 'b': out += "1011"; break;
+// 				case 'c': out += "1100"; break;
+// 				case 'd': out += "1101"; break;
+// 				case 'e': out += "1110"; break;
+// 				case 'f': out += "1111"; break;
+// 				default: return "";
+// 			}
+// 		}
+// 		return this.binFormat(out);
+// 	}
+// 	bin2hex(bin){
+// 		// console.log('pre-format:',bin)
+// 		bin = this.binFormat(bin.slice())
+// 		// console.log('post-format:',bin)
+// 		var out = "";
+// 		var accumulator=''
+// 		for(var c = 1; c<=bin.length; c++) {
+// 			accumulator+=bin[c-1]
+// 			if(c%4==0){
+// 				switch(accumulator) {
+// 					case "0000": out += '0'; break;
+// 					case "0001": out += '1'; break;
+// 					case "0010": out += '2'; break;
+// 					case "0011": out += '3'; break;
+// 					case "0100": out += '4'; break;
+// 					case "0101": out += '5'; break;
+// 					case "0110": out += '6'; break;
+// 					case "0111": out += '7'; break;
+// 					case "1000": out += '8'; break;
+// 					case "1001": out += '9'; break;
+// 					case "1010": out += 'a'; break;
+// 					case "1011": out += 'b'; break;
+// 					case "1100": out += 'c'; break;
+// 					case "1101": out += 'd'; break;
+// 					case "1110": out += 'e'; break;
+// 					case "1111": out += 'f'; break;
+// 					default: return "";
+// 				}
+// 				// console.log(accumulator)
+// 				accumulator=""
+// 			}
+// 		}
+// 		// console.log(out)
+// 		return this.strip(out);
+// 	}
+// 	bin2Decimal(bin){
+// 		//start from the right
+// 		bin=this.binFormat(bin.slice())
+// 		var i = bin.length-1;
+// 		var decimal=0
+// 		var j = 0;
+// 		while(i>=0){
+// 			if(bin[i]=='1'){
+// 				decimal+=Math.pow(2, j)
+// 			}
+// 			i--
+// 			j++
+// 		}
+// 		return decimal
+// 	}
 
-	setCharAt(str, index, chr) {
-		return str.substring(0, index) + chr + str.substring(index+1);
+
+
+// 	setCharAt(str, index, chr) {
+// 		return str.substring(0, index) + chr + str.substring(index+1);
+// 	}
+
+// 	decimal2Hex(decimal){
+// 		return this.strip(this.bin2hex(this.decimal2Bin(decimal)))
+// 	}
+// 	hex2Decimal(hex){
+// 		hex=hex.slice()
+// 		return this.bin2Decimal(this.binFormat(this.hex2bin(hex)))
+// 	}
+
+
+// 	hex2String(hex){
+// 		hex=hex.slice()
+// 		var buffer = this.hex2Buffer(this.strip(hex))
+// 		var string = this.buffer2String(buffer)
+// 		return string
+// 	}
+// 	string2Buffer(string){
+// 		string=string.slice()
+// 		// console.log(Buffer.from(string))
+// 		return Buffer.from(string)
+// 	}
+
+// 	buffer2Hex(buffer){
+// 		return  this.strip(buffer.toString('hex'))
+// 	}
+// 	buffer2String(buffer) {
+// 		buffer=buffer.subarray()
+// 		return this._buffer2String(buffer)
+// 	}
+// 	hex2Buffer(hex){
+// 		hex = hex.slice()
+// 		return Buffer.from(this.strip(hex), 'hex')
+// 	}
+// 	_buffer2String(buffer){
+// 		var str=''
+// 		for (let pair of buffer.entries()) {
+// 			// get the byte of character
+// 			const charCode = pair[1];
+// 			// use String.fromCharCode() to get the appropriate character
+// 			// for the byte
+// 			str+=String.fromCharCode(charCode)
+// 		  }
+// 		return str
+// 	}
+// 	// stripBuffer(buffer){
+// 	// 	buffer=buffer.subarray()
+// 	// 	var i =0;
+// 	// 	while(true){
+// 	// 		if(buffer[i]=='0'){
+// 	// 			buffer = buffer.slice(1)
+// 	// 			i--
+// 	// 		}else{
+// 	// 			break
+// 	// 		}
+// 	// 		i++
+// 	// 	}
+
+// 	// 	return buffer
+// 	// }
+
+
+// 	binFormat(bin){
+// 		bin=bin.slice()
+// 		bin = this.strip(bin)
+// 		if(bin.length%8==0){
+// 			return bin
+// 		}
+// 		while(true){
+// 			if(bin.length%8==0){
+// 				break
+// 			}else{
+// 				bin='0'.concat(bin)
+// 			}
+// 		}
+// 		return bin
+// 	}
+
+// 	strip(str){
+// 		str=str.slice()
+// 		var i =0;
+// 		while(true){
+// 			if(str[i]=='0'){
+// 				str = str.slice(1)
+// 				i--
+// 			}else{
+// 				return str
+// 			}
+// 			i++
+// 		}
+// 		return str
+// 	}
+// 	hexFormat(hex, n){
+// 		hex=hex.slice()
+// 		hex = this.strip(hex)
+// 		while(true){
+// 			if(hex.length%n==0){
+// 				break
+// 			}else{
+// 				hex='0'.concat(hex)
+// 			}
+// 		}
+// 		return hex
+// 	}
+
+// 	hex(n){
+// 		var hex = this.bin2hex(this.nibbles(n))
+// 		return this.strip(hex)
+// 	}
+// 	buffer(n){
+// 		return new Utils().hex2Buffer(this.hex(n))
+// 	}
+
+	nibbleRange(min, max){
+		//returns a random nibble between min and max
+		//which are decimals, every nibble is 0 to 16
+		//so we can use randomRange(min, max), so long as min
+		//and max are greater than or equal to 0, and less
+		//than or equal to 16
+		if(min<0||max>16||min>max){
+			throw Error('nibble range error: decimals points must be within nibble range 0-16')
+		}
+		//decimal to bin always returns byte strings that represent the decimal number of any size
+		return new Encoding().nibbleFormatBE(this.decimal2binBE(this.randomRange(min, max))) 
 	}
 
-	decimal2Hex(decimal){
-		return this.strip(this.bin2hex(this.decimal2Bin(decimal)))
-	}
-	hex2Decimal(hex){
-		hex=hex.slice()
-		return this.bin2Decimal(this.binFormat(this.hex2bin(hex)))
-	}
-
-	string2Hex(string){
-		string=string.slice()
-		return this.strip(this.buffer2Hex(this.string2Buffer(string)))
-	}
-	hex2String(hex){
-		hex=hex.slice()
-		var buffer = this.hex2Buffer(this.strip(hex))
-		var string = this.buffer2String(buffer)
-		return string
-	}
-	string2Buffer(string){
-		string=string.slice()
-		// console.log(Buffer.from(string))
-		return Buffer.from(string)
-	}
-
-	buffer2Hex(buffer){
-		return  this.strip(buffer.toString('hex'))
-	}
-	buffer2String(buffer) {
-		buffer=buffer.subarray()
-		return this._buffer2String(buffer)
-	}
-	hex2Buffer(hex){
-		hex = hex.slice()
-		return Buffer.from(this.strip(hex), 'hex')
-	}
-	_buffer2String(buffer){
-		var str=''
-		for (let pair of buffer.entries()) {
-			// get the byte of character
-			const charCode = pair[1];
-			// use String.fromCharCode() to get the appropriate character
-			// for the byte
-			str+=String.fromCharCode(charCode)
-		  }
-		return str
-	}
-	// stripBuffer(buffer){
-	// 	buffer=buffer.subarray()
-	// 	var i =0;
-	// 	while(true){
-	// 		if(buffer[i]=='0'){
-	// 			buffer = buffer.slice(1)
-	// 			i--
-	// 		}else{
-	// 			break
+	// 	bytes(n){
+	// 		var bytes=''
+	// 		for(var i = 0; i<n; i++){
+	// 			for(var j = 0; j<8; j++){
+	// 				bytes+=new Rand().mod10()
+	// 			}
 	// 		}
-	// 		i++
+	// 		return this.binFormat(bytes)
 	// 	}
-
-	// 	return buffer
-	// }
-
-
-	binFormat(bin){
-		bin=bin.slice()
-		bin = this.strip(bin)
-		if(bin.length%8==0){
-			return bin
-		}
-		while(true){
-			if(bin.length%8==0){
-				break
-			}else{
-				bin='0'.concat(bin)
-			}
-		}
-		return bin
-	}
-
-	strip(str){
-		str=str.slice()
-		var i =0;
-		while(true){
-			if(str[i]=='0'){
-				str = str.slice(1)
-				i--
-			}else{
-				return str
-			}
-			i++
-		}
-		return str
-	}
-	hexFormat(hex, n){
-		hex=hex.slice()
-		hex = this.strip(hex)
-		while(true){
-			if(hex.length%n==0){
-				break
-			}else{
-				hex='0'.concat(hex)
-			}
-		}
-		return hex
-	}
-
-	hex(n){
-		var hex = this.bin2hex(this.nibbles(n))
-		return this.strip(hex)
-	}
-	buffer(n){
-		return new Utils().hex2Buffer(this.hex(n))
-	}
-
-	nibbles(n){
-		var nibbles=''
-		for(var i = 0; i<n; i++){
-			for(var j = 0; j<4; j++){
-				nibbles+=new Rand().mod10()
-			}
-		}
-		return nibbles
-	}
-	bytes(n){
-		var bytes=''
-		for(var i = 0; i<n; i++){
-			for(var j = 0; j<8; j++){
-				bytes+=new Rand().mod10()
-			}
-		}
-		return this.binFormat(bytes)
-	}
 }
